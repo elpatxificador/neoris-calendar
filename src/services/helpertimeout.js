43c115882204a702db22d4de965178a2ea7e4ef3
Jesús Ángel Sánchez Sánchez <jesusangel.sanchez.contractor@bbva.com>
2019-07-26 14:15:47 +0200

    [neoris-calendar] Added comments.

--- src/services/helpertimeout.js
+++ src/services/helpertimeout.js
@@ -1,7 +1,16 @@
+//Create function and call function with self (window||global) for testing (mock)
 export const interval = (function(scope) {
     return {
         setInterval: scope.setInterval,
         clearInterval: scope.clearInterval,
     }
     
-    }(self));
+}(self));
+
+/*
+
+MOCK:
+
+interval.setInterval=()=>1;
+interval.clearInterval=()=>0;
+*/
