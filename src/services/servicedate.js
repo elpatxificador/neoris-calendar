b704de6e33b666dc94cab54ca37ef0c0f88eb20a
Jesús Ángel Sánchez Sánchez <jesusangel.sanchez.contractor@bbva.com>
2019-07-26 13:51:16 +0200

    [neoris-calendar] Refactor helperday.

--- src/services/servicedate.js
+++ src/services/servicedate.js
@@ -1,7 +1,37 @@
-export class ServiceDate{
 
-    static addMonth(date, value){}
-    static addSecond(date, value){}
+function setInterval(window, value) {
+    window.setInterval=(value)=>{}
+}(window||global)
+
+function formatDate(date, format) {
+
 }
 
-export const DateSystem = new Date();
+// export const DateSystem = new Date();
+// export const MONTHS = [31,28,31,30,31,];
+
+/* 
+let x = (function foo(a){
+    setInterval=()=>{	
+    }(window||global))
+
+function addSecond(date, value) {
+    myVar = setInterval(alertFunc, 3000);
+}(window||global)
+
+
+function renderDay(Day) {
+
+    html`<span>day.day</span>`
+} 
+
+function* range(to, from) {
+    for (var i=to; i<from; i++) {
+        yield i;
+    }
+}
+
+[...range(1, 31)].map()
+
+*/
+
