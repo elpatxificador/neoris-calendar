f6782ee3d9d72a64b5cf2612f5a643833a389ee2
Jesús Ángel Sánchez Sánchez <jesusangel.sanchez.contractor@bbva.com>
2019-07-26 12:40:13 +0200

    [neoris-calendar] Added clock component.

--- src/components/clock.js
+++ src/components/clock.js
@@ -1,1 +1,25 @@
 import { LitElement, html } from 'lit-element';
+
+class Clock extends LitElement {
+
+    constructor() {
+        super();
+    }
+    
+    _render() {
+        return html`<p>Soy un parrafo</p>`;
+    }
+
+    connectedCallback() {
+        this._render();
+    }
+
+    attributeChangedCallback(attrName, oldVal, newVal) {
+        console.log(oldVal);
+        console.log(newVal);
+        //Si mi atributo/propiedad esta definido luego asigno a ese atributo un valor
+        (attrName in this) && (this[attrName] = newVal);
+    }
+}
+    
+customElements.define('neoris-clock', Clock);
