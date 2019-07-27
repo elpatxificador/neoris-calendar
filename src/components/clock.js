cff6071b33052dad1bd8fadcd57664744db1a3cf
Javier García Riesco <javier.garcia.riesco.contractor@bbva.com>
2019-07-27 13:13:28 +0200

    Fix clock renderization

--- src/components/clock.js
+++ src/components/clock.js
@@ -7,29 +7,32 @@ class Clock extends LitElement {
 
     constructor() {
         super();
-
         this._intervalID = 0;
         this._time = undefined;
     }
     
     render() {
         console.log(this._time);
-        return html`<p>Reloj: ${this._time}</p>`;
+        if (!this._time) {
+            this._time = dateSystem.getHours() + ":" + dateSystem.getMinutes() + ":" + dateSystem.getSeconds();
+        }
+        return html`<p>${this._time}</p>`;
     }
 
     connectedCallback() {
+        super.connectedCallback();
         this._intervalID = interval.setInterval(this.changeClock.bind(this), tick);
     }
 
     disconnectedCallback() {
+        super.disconnectedCallback();
         interval.clearInterval(this._intervalID);
     }
 
     changeClock() {
-        
         dateSystem.setSeconds(dateSystem.getSeconds() + (tick/1000));
         this._time = dateSystem.getHours() + ":" + dateSystem.getMinutes() + ":" + dateSystem.getSeconds();
-        this.render();
+        this.requestUpdate();
     }
 }
     
